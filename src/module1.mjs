3f432ef46d59af16b0a1c42441934de0dddabcfd
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-26 04:06:07 +0530

    props

---
+++ src/module1.mjs
@@ -0,0 +1,4 @@
+import ui from './module2.mjs';
+//use any name to import default modules
+//use exact name and {} to import non-default moudules/components
+console.log(ui);
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+const a = "Harry";
+const b = "Rakesh";
+const c = "Durgesh";
+const d = "Manoj";
+
+export default c;
